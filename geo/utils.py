036f0eac4f0f46cf9a4f04d5ad0a92cd2367f2ac
jin-156 <bw1203j@naver.com>
2025-11-16 22:08:56 +0900

    Lab11 complete2

--- geo/utils.py
+++ geo/utils.py
@@ -1,4 +1,9 @@
-# geo/utils.py
+import math
 
-def add(a, b):
-    return a + b
+def pythagoras(a, b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+def circle(r):
+    area = math.pi * r**2
+    return area
--- tester.py
+++ tester.py
@@ -1,3 +1,11 @@
-from geo.utils import add
+import geo.utils as utils
 
-print(add(3, 5))
+# calculate the length of hypotenuse(c) when a=3 and b=4
+a, b = 3, 4
+c = utils.pythagoras(a, b)
+print('c =', c)
+
+# calculate the area of circle with radius r = 10
+r = 10
+area = utils.circle(r)
+print('area =', area)
